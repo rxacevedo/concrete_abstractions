cc92217f06cd46799a3c19ca32be2a7951133886
Roberto Acevedo <rxacevedo@gmail.com>
2013-08-10 14:14:37 -0400

    Some more problems

--- ch2.lisp
+++ ch2.lisp
@@ -150,3 +150,100 @@
 (defun find-exp-2 (n)
 	(cond ((oddp n) 0)
 		  (t (1+ (find-exp-2 (/ n 2))))))
+
+; Exercise 2.16
+; Use induction to prove that (foo x n) terminates with the value
+;
+; x^(n + 1) - 1
+; -------------
+; 	  x - 1
+;
+
+(defun foo (x n)
+	(if (eq n 0) 0
+		(+ (expt x n) (foo x (- n 1)))))
+
+; Base case: (foo x 0) terminates with the value 1 because of the evaluation rule if. 
+; Becuase... 
+;
+; x^(1) - 1 
+; ---------
+;   x - 1 
+;
+; is the same as (x - 1) / (x - 1), which is the same as 1, (foo x 0) computes the correct value.
+;
+; Induction hypothesis: Assume that (foo x k) terminates with the value of (x^(k + 1) -1) / (x - 1) for all
+; k in the range 0 ≤ k < n.
+;
+; Inductive step: Consider evaluating (foo x n), with n > 0. This will terminate if the evaluation of
+; (foo x (- n 1)) terminates and will have the same value as (+ (expt x n) (foo x (- n 1))). Because (- n 1)
+; evaluates to n - 1, and 0 ≤ n - 1 < n, we can therefore assume by our induction hypothesis that 
+; (foo x (- n 1)) does terminate, with the value of x^((n - 1) + 1) - 1 / x - 1, which simplifies to
+; (x^n - 1) / x - 1. Because...
+;
+; ((x^((n-1) + 1) - 1) / x - 1) + x^n 
+; = (x^n - 1 / x - 1) + x^n
+; = (x^n - 1 / x - 1) + (x-1)x^n / x - 1
+; = (x^n - 1 / x - 1) + (x^(n + 1) - x^n / x - 1)
+; = (x^n - 1 + x^(n + 1) - x^n) / x - 1
+; = (x^n - x^n + x^(n + 1) - 1) / x - 1
+; = (x^(n + 1) - 1) / x - 1
+; ...we see that (foo x n) does terminate with the correct value for any arbitrary positive n, under the 
+; inductive hypothesis of correct operation for smaller arguments.
+
+
+; Exercise 2.17
+;
+; Perhaps you have heard the following Christmas song:
+;
+; On the first day of Christmas My true love gave to me
+; A partridge in a pear tree.
+;
+; On the second day of Christmas My true love gave to me
+; Two turtle doves
+; And a partridge in a pear tree.
+;
+; On the third day of Christmas My true love gave to me Three French hens,
+; Two turtle doves,
+; And a partridge in a pear tree.
+;
+; Given:
+
+(defun presents-on-day (n)
+	(if (eq n 1) 1
+		(+ n (presents-on-day (- n 1)))))
+
+; Write procedure (presents-through-day n), using (presents-on-day n)
+
+(defun presents-through-day (n)
+	(if (eq n 1) 1
+		(+ (presents-on-day n) (presents-through-day (- n 1)))))
+
+
+; --------------------------------------------------------------------------------------------------------
+
+; Exercise 2.18
+; Prove by induction that for every nonnegative integer n the following procedure computes 2n:
+  
+(defun f (n)
+	(if (eq n 0) 0
+		(+ 2 (f (- n 1)))))
+
+; Base case: (f 0) terminates with the value of 0 because of the evaluation rule if.
+; Because 0 = 2(0), (f 0) computes the correct value.
+;
+; Induction hypothesis: Assume that (f k) terminates with the value 2k for all k in the range 0 ≤ k < n
+;
+; Inductive step: Consider evaluating (f n), with n > 0. This will terminate if 
+; the evaluation of (f (- n 1)) terminates and will have the same value as (+ 2 (f (- n 1))). Becuase 
+; (- n 1) evaluates to n - 1, and 0 ≤ n - 1 < n, we can therefore assume by our induction hypothesis that
+; (f (- n 1)) does terminate with the value 2n. Therefore, (+ 2 (f (- n 1))) evaluates to 2 + 2(n - 1). 
+; Becuase ...
+; 2n = 2 + 2(n - 1)
+; 2n = 2 + 2n - 2
+; 2n = 2n
+; ...we see that (f n) does terminate with the correct value for any arbitrary positive n, under the inductive
+; hypothesis of correct operation for smaller arguments.
+;
+; Conclusion: Therefore, by mathematical induction on n, (f n) terminates with the value 2n for any
+; nonnegative integer n.
